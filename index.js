ede074793997391a7dd5b1b0487ec8b15c4a2851
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2022-12-08 07:30:50 -0500

    coolio
    coolio

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function No() {
     document.getElementById("o1").innerHTML = "get scamed lol!!!!"
 }
+
+function venison() {
+    console.log("ygifudhhby89ifd")
+    document.getElementById("deer").innerHTML = "+1 Raw Venison"
+}
