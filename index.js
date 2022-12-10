560229068e1341097cec2774b2ec7be898c03406
Poley Kid <juicetin.kim@gmail.com>
2022-12-10 13:40:48 -0500

    jquery update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function No() {
-    document.getElementById("o1").innerHTML = "get scamed lol!!!!"
+    $("#o1").html("<p>get scamed lol!!!!</p>") 
 }
 
 function venison() {
     console.log("ygifudhhby89ifd")
-    document.getElementById("deer").innerHTML = "+1 Raw Venison"
+    $("#deer").html("<h1>+1 Raw Venison</h1>")
 }
